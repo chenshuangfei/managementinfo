db00e7eb9b305b9d14326333d391a32ad7ea659e
chenshuangfei <chenshaungfei@njsdata.com>
2019-02-22 14:31:09 +0800

    perso添加

--- src/main/java/com/chen/entity/Person.java
+++ src/main/java/com/chen/entity/Person.java
@@ -5,6 +5,16 @@ public class Person {
 
     private String studentNn;
 
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public String getStudentNn() {
         return studentNn;
     }
