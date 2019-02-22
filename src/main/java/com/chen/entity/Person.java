c33d70a2b9a5b79ab1a469cf0207c3f1da1dfae9
chenshuangfei <chenshaungfei@njsdata.com>
2019-02-22 14:29:30 +0800

    提交person添加age

--- src/main/java/com/chen/entity/Person.java
+++ src/main/java/com/chen/entity/Person.java
@@ -5,6 +5,16 @@ public class Person {
 
     private String studentNn;
 
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
     public String getStudentNn() {
         return studentNn;
     }
