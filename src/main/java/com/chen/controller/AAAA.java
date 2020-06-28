27ada8887ea980b244387c48090996f0ed5bc1ec
chensf3 <chensf3@asiainfo.com>
2020-06-28 16:43:00 +0800

    src/main/java/com/chen/controller/AAAA.java

--- src/main/java/com/chen/controller/AAAA.java
+++ src/main/java/com/chen/controller/AAAA.java
@@ -2,6 +2,6 @@ package com.chen.controller;
 
 public class AAAA {
     public static void main(String[] args) {
-        System.out.println("1111");
+        System.out.println("fafadfad");
     }
 }
